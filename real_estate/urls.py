7c559056f7d07b96b66b2fee751ec07e27ff0308
Roman <tesslogun@gmail.com>
2023-02-13 11:09:30 +0300

    add custom user model, integrate it in admin panel

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('superuser/', admin.site.urls),
+    path("superuser/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Custom bookstore admin"
+admin.site.site_title = "Custom bookstore admin site"
+admin.site.index_title = "Custom Bookstore Admin"
